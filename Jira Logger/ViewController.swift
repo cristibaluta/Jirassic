ea938c46071488b1530d02b1dfaf50c43d058ad3
Baluta Cristian <cristianbal@admins-Mac-mini-4.local>
2015-03-24 14:57:33 +0200

    Initial Commit

---
+++ Jira Logger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jira Logger
+//
+//  Created by Cristian Baluta on 24/03/15.
+//  Copyright (c) 2015 Cristian Baluta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
