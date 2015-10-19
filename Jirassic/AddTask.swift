30048433f58d9741da279fdfe49bdbd1f81d72e9
Baluta Cristian <cristianbal@Cristis-Mac-mini.local>
2015-10-19 18:15:33 +0300

    ...

---
+++ Jirassic/AddTask.swift
@@ -0,0 +1,13 @@
+//
+//  AddTask.swift
+//  Jirassic
+//
+//  Created by Cristian Baluta on 19/10/15.
+//  Copyright © 2015 Cristian Baluta. All rights reserved.
+//
+
+import Cocoa
+
+class AddTask: NSObject {
+
+}
