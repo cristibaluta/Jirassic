3617a4d70e8eb533b469763b8f08bf971cda595e
Cristian Baluta <cristi.baluta@gmail.com>
2015-11-25 18:53:23 +0200

    Added command line target

---
+++ Jirrasic-Line/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Jirrasic-Line
+//
+//  Created by Cristian Baluta on 25/11/15.
+//  Copyright © 2015 Cristian Baluta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
