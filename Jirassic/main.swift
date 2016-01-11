900e3016c9e98e76192b7b71ae0de8fc71b815c9
Baluta Cristian <cristi.baluta@gmail.com>
2016-01-11 13:40:38 +0200

    Add jirassic cli

---
+++ Jirassic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  jirassic
+//
+//  Created by Baluta Cristian on 06/01/16.
+//  Copyright © 2016 Cristian Baluta. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
