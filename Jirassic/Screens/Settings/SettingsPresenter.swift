3ba8675b2048b47a17631c1cf6f77ecaae19b3dd
Baluta Cristian <cristi.baluta@gmail.com>
2016-05-02 08:55:32 +0300

    add settings presenter

---
+++ Jirassic/Screens/Settings/SettingsPresenter.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsPresenter.swift
+//  Jirassic
+//
+//  Created by Cristian Baluta on 02/05/16.
+//  Copyright © 2016 Cristian Baluta. All rights reserved.
+//
+
+import Foundation
+
+protocol SettingsPresenterInput {
+    
+    
+}
+
+protocol SettingsPresenterOutput {
+    
+    
+}
+
+class SettingsPresenter {
+    
+}
+
+extension SettingsPresenter: SettingsPresenterInput {
+    
+}
