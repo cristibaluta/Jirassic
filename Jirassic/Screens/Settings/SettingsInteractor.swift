0ccde169145fe571f5e3d2a294e7b56a0e20aabc
Baluta Cristian <cristi.baluta@gmail.com>
2016-09-06 09:45:07 +0300

    Added SettingsInteractor that will give information about the state of the settings

---
+++ Jirassic/Screens/Settings/SettingsInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsInteractor.swift
+//  Jirassic
+//
+//  Created by Cristian Baluta on 06/09/16.
+//  Copyright © 2016 Cristian Baluta. All rights reserved.
+//
+
+import Foundation
+
+class SettingsInteractor: NSObject {
+
+}
