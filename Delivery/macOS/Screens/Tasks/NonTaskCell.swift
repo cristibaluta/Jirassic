083cda1691c4f55b227229b40a346fb776b1145f
Cristian Baluta <cristian.baluta@fortech.ro>
2016-11-28 13:49:47 +0200

    Made NonTaskCell editable

--- Delivery/macOS/Screens/Tasks/NonTaskCell.swift
+++ Delivery/macOS/Screens/Tasks/NonTaskCell.swift
@@ -16,8 +16,11 @@ class NonTaskCell: NSTableRowView, CellProtocol {
 	@IBOutlet fileprivate var notesTextFieldTrailingContraint: NSLayoutConstraint?
 	@IBOutlet fileprivate var butRemove: NSButton?
 	@IBOutlet fileprivate var butAdd: NSButton?
-	
-	fileprivate var trackingArea: NSTrackingArea?
+    
+    fileprivate var isEditing = false
+    fileprivate var wasEdited = false
+    fileprivate var trackingArea: NSTrackingArea?
+    var _dateEnd = ""
 	
 	var didEndEditingCell: ((_ cell: CellProtocol) -> ())?
 	var didRemoveCell: ((_ cell: CellProtocol) -> ())?
@@ -25,12 +28,15 @@ class NonTaskCell: NSTableRowView, CellProtocol {
 	var didCopyContentCell: ((_ cell: CellProtocol) -> ())?
 	var data: TaskCreationData {
 		get {
+            let hm = Date.parseHHmm(self.dateEndTextField!.stringValue)
+            let date = Date().dateByUpdating(hour: hm.hour, minute: hm.min)
 			return (dateStart: nil,
-			        dateEnd: Date(),
-			        notes: "",
-					self.notesTextField!.stringValue)
+			        dateEnd: date,
+			        taskNumber: "",
+			        notes: self.notesTextField!.stringValue)
 		}
-		set {
+        set {
+            _dateEnd = newValue.dateEnd.HHmm()
 			self.dateEndTextField?.stringValue = newValue.dateEnd.HHmm()
 			self.notesTextField?.stringValue = newValue.notes
 		}
@@ -74,14 +80,16 @@ class NonTaskCell: NSTableRowView, CellProtocol {
 	
 	override func mouseEntered (with theEvent: NSEvent) {
 		self.butRemove?.isHidden = false
-		self.butAdd?.isHidden = false
+        self.butAdd?.isHidden = false
+        self.dateEndTextField?.isEditable = true
 		self.notesTextFieldTrailingContraint?.constant = 80
 		self.setNeedsDisplay(self.frame)
 	}
 	
 	override func mouseExited (with theEvent: NSEvent) {
 		self.butRemove?.isHidden = true
-		self.butAdd?.isHidden = true
+        self.butAdd?.isHidden = true
+        self.dateEndTextField?.isEditable = false
 		self.notesTextFieldTrailingContraint?.constant = 10
 		self.setNeedsDisplay(self.frame)
 	}
@@ -108,4 +116,41 @@ class NonTaskCell: NSTableRowView, CellProtocol {
 			self.addTrackingArea(self.trackingArea!)
 		}
 	}
+}
+
+extension NonTaskCell: NSTextFieldDelegate {
+    
+    override func controlTextDidBeginEditing (_ obj: Notification) {
+        isEditing = true
+    }
+    
+    override func controlTextDidChange (_ obj: Notification) {
+        wasEdited = true
+        if obj.object as? NSTextField == dateEndTextField {
+            let predictor = PredictiveTimeTyping()
+            let comps = dateEndTextField!.stringValue.components(separatedBy: _dateEnd)
+            let newDigit = (comps.count == 1 && _dateEnd != "") ? "" : comps.last
+            _dateEnd = predictor.timeByAdding(newDigit!, to: _dateEnd)
+            dateEndTextField?.stringValue = _dateEnd
+        }
+    }
+    
+    override func controlTextDidEndEditing (_ obj: Notification) {
+        if wasEdited {
+            wasEdited = false
+            self.didEndEditingCell?(self)
+        }
+    }
+    
+    func control (_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
+        
+        if control as? NSTextField == dateEndTextField {
+            RCLog(commandSelector)
+            if wasEdited && commandSelector == #selector(NSResponder.insertNewline(_:)) {
+                self.didEndEditingCell?(self)
+                wasEdited = false
+            }
+        }
+        return false
+    }
 }
