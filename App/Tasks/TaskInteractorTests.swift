55dbfcea0c04187d1db3580822e4410a1e702afa
Baluta Cristian <cristi.baluta@gmail.com>
2018-10-24 08:05:50 +0300

    Fix tests compilation error

--- App/Tasks/TaskInteractorTests.swift
+++ App/Tasks/TaskInteractorTests.swift
@@ -16,19 +16,19 @@ class TaskInteractorTests: XCTestCase {
         let repository = InMemoryCoreDataRepository()
         let interactor = TaskInteractor(repository: repository, remoteRepository: nil)
         
-        let tasksBeforeInsert = repository.queryTasksInDay(Date())
+        let tasksBeforeInsert = repository.queryTasks(startDate: Date().startOfDay(), endDate: Date().endOfDay())
         XCTAssert(tasksBeforeInsert.count == 0, "We added one task, we should receive one task")
         
         let task = Task(endDate: Date(), type: TaskType.issue)
         interactor.saveTask(task, allowSyncing: false, completion: { task in })
         
-        let tasks = repository.queryTasksInDay(Date())
+        let tasks = repository.queryTasks(startDate: Date().startOfDay(), endDate: Date().endOfDay())
         XCTAssert(tasks.count == 1, "We added one task, we should receive one task")
         
         let taskToDelete = tasks.first!
         interactor.deleteTask(taskToDelete)
         
-        let tasksAfterDelete = repository.queryTasksInDay(Date())
+        let tasksAfterDelete = repository.queryTasks(startDate: Date().startOfDay(), endDate: Date().endOfDay())
         XCTAssert(tasksAfterDelete.count == 0, "There should be no tasks left")
     }
 }
