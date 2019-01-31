f9b9363a7f293f5ca4c7634c4ddb1ae97bc964c0
Baluta Cristian <cristi.baluta@gmail.com>
2019-01-31 09:13:29 +0200

    ...

--- Delivery/macOS/Screens/Projects/ProjectsViewController.swift
+++ Delivery/macOS/Screens/Projects/ProjectsViewController.swift
@@ -9,5 +9,35 @@
 import Cocoa
 
 class ProjectsViewController: NSViewController {
+    @IBOutlet private weak var scrollView: NSScrollView!
+    @IBOutlet private weak var tableView: NSTableView!
+    var projects = ["Bosch Ebike", "Electric castle"]
+}
+
+extension ProjectsViewController: NSTableViewDataSource {
     
+    func numberOfRows (in aTableView: NSTableView) -> Int {
+        return projects.count
+    }
+}
+
+extension ProjectsViewController: NSTableViewDelegate {
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        
+        if (tableColumn?.identifier)?.rawValue == "isSelected" {
+            return nil
+        }
+        if (tableColumn?.identifier)?.rawValue == "name" {
+            return projects[row]
+        }
+        return nil
+    }
+    
+    func tableView(_ tableView: NSTableView, setObjectValue object: Any?, for tableColumn: NSTableColumn?, row: Int) {
+        
+        if (tableColumn?.identifier)?.rawValue == "isSelected" {
+            
+        }
+    }
 }
